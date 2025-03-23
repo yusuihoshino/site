f10f94d7bc918212be28e61fb06449d9a049c83f
YusuiHoshino <mazisu99@gmail.com>
2025-03-24 00:03:47 +0900

    fix

--- components.js
+++ components.js
@@ -8,8 +8,8 @@ customElements.define('sidebar-comp', class extends HTMLElement {
 
             <div class="sidebar-group">
                 <a href="../history/index.html">History</a>
-                <a href="../portfolio/index.html">Portfolio</a>
                 <a href="../request/index.html">Request</a>
+                <a href="../portfolio/index.html">Portfolio</a>
                 <a href="../product/index.html">Product</a>
                 <a href="../poster/index.html">Poster</a>
                 <a href="../logo/index.html">Logo</a>
@@ -17,9 +17,9 @@ customElements.define('sidebar-comp', class extends HTMLElement {
             </div>
             
             <div class="sidebar-group">
-                <a href="../thumnail/index.html">Thumnail</a>
                 <a href="../photo/index.html">Photo</a>
                 <a href="../drawing/index.html">Drawing</a>
+                <a href="../thumnail/index.html">Thumnail</a>
                 <a href="../modeling/index.html">Modeling</a>
                 <a href="../art/index.html">Art</a>
             </div>
@@ -57,6 +57,50 @@ customElements.define('card-comp', class extends HTMLElement {
       this.innerHTML = `
         <a class="grid-item" href="${link}">
           <img src="${image}" alt="${title}">
+          <div class="grid-item-below">
+            <div class="grid-item-texts">
+              <p>${title}</p>
+              <div class="line"></div>    
+              <p>${date}</p> 
+            </div>
+            <p>detail</p>
+          </div>
+        </a>
+      `;
+    } else {
+      // リンクなしの場合（aタグではなくdivタグ）
+      this.innerHTML = `
+        <div class="grid-item">
+          <img src="${image}" alt="${title}">
+          <div class="grid-item-below">
+            <div class="grid-item-texts">
+              <p>${title}</p>
+              <div class="line"></div>    
+              <p>${date}</p> 
+            </div>
+          </div>
+        </div>
+      `;
+    }
+  }
+});
+
+customElements.define('y-card-comp', class extends HTMLElement {
+  constructor() {
+    super();
+
+    // 属性を取得
+    const image = this.getAttribute('image') || '';
+    const link = this.getAttribute('link'); // nullになる可能性あり
+    const title = this.getAttribute('title') || 'no title';
+    const date = this.getAttribute('date') || '----/--';
+
+    // 中身を描画（リンクの有無で分岐）
+    if (link) {
+      // リンクありの場合
+      this.innerHTML = `
+        <a class="grid-item" href="${link}">
+          <iframe width="560" height="315" src="${image}" allowfullscreen></iframe>
           <div class="grid-item-below">
             <div class="grid-item-texts">
               <p>${title}</p>

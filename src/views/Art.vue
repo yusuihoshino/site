862dce05b3c7e198370f87b9dfcab71b900c2ee1
YusuiHoshino <mazisu99@gmail.com>
2025-03-24 23:05:34 +0900

    大改造前

--- src/views/Art.vue
+++ src/views/Art.vue
@@ -1,79 +1,18 @@
 <template>
-<div>
-   <div class="galary-container container" id="container">
-    <TitlePage
-      title="Art"
-      description="アート作品"
-    />
-    <Gallery :items="artworks" />
-  </div>
-  <Sidebar />
-</div>
+  <GalleryBase
+    pageTitle="Art"
+    pageDescription="デジタルアート作品"
+    category="art"
+  />
 </template>
 
 <script>
-import Sidebar from '@/components/Sidebar.vue';
-import TitlePage from '@/components/Title.vue';
-import Gallery from '@/components/Gallery.vue';
+import GalleryBase from '@/components/GalleryBase.vue';
 
 export default {
   name: 'ArtPage',
   components: {
-    Sidebar,
-    TitlePage,
-    Gallery
-  },
-  data() {
-    return {
-      artworks: [
-        {
-          title: 'デジタルアート1',
-          year: '2024',
-          image: require('@/assets/img/art/ae1.webp'),
-          link: '/drawing/SpainRyokou'
-        },
-        {
-          title: 'デジタルアート2',
-          year: '2024',
-          image: require('@/assets/img/art/ae2.webp'),
-        },
-        {
-          title: 'デジタルアート3',
-          year: '2023',
-          image: require('@/assets/img/art/ae3.png'),
-        },
-        {
-          title: 'デジタルアート4',
-          year: '2023',
-          image: require('@/assets/img/art/ae4.png'),
-        },
-        {
-          title: 'デジタルアート5',
-          year: '2023',
-          image: require('@/assets/img/art/ae5.png')
-        },
-        {
-          title: 'デジタルアート6',
-          year: '2023',
-          image: require('@/assets/img/art/ae6.webp')
-        },
-        {
-          title: 'Processing作品1',
-          year: '2023',
-          image: require('@/assets/img/art/p5-1.webp'),
-        },
-        {
-          title: 'Processing作品2',
-          year: '2023',
-          image: require('@/assets/img/art/p5-2.png'),
-        },
-        {
-          title: 'ジャケットデザイン',
-          year: '2024',
-          image: require('@/assets/img/art/ジャケット.webp'),
-        }
-      ]
-    };
+    GalleryBase
   }
 };
 </script>
